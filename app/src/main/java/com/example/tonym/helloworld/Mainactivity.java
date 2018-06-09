11738c1ca19cbd37d3165fad4eae3ba82e5cac5d
tonymoturi <tonymoturi@gmail.com>
2018-06-09 13:06:11 +0300

    first commit

---
+++ app/src/main/java/com/example/tonym/helloworld/Mainactivity.java
@@ -0,0 +1,13 @@
+package com.example.tonym.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mainactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+    }
+}
